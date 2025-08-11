3c8ff6d19e2fd3951ad0064efe1feecaeb718133
Mohammed Farhan Hussain <farhanleo007@gmail.com>
2025-08-11 22:31:20 +0530

    firebase setup

--- next.config.js
+++ next.config.js
@@ -6,17 +6,7 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
-          { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
-        ],
-      },
-    ];
-  },
+  // Removed headers() function as it doesn't work with static export
 };
 
 module.exports = nextConfig;
